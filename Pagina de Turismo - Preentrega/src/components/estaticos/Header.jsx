d8855773e45a0b246103b4ad7a9bd51cb328f73a
Analia-Elizabeth-Fernandez <anfer510@gmail.com>
2026-01-07 13:44:40 -0300

    Update Header.jsx

--- Pagina de Turismo - Preentrega/src/components/estaticos/Header.jsx
+++ Pagina de Turismo - Preentrega/src/components/estaticos/Header.jsx
@@ -1,26 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import Cart from '../Cart';
+import { CartContext } from '../../context/CartContext';
 import './styleEstatico.css';
-import { CartContext } from '../../context/CartContext'; // tu contexto del carrito
 
 const Header = () => {
-  const { cart } = useContext(CartContext); // para mostrar la cantidad de items
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const { cart } = useContext(CartContext);
 
   return (
     <header>
       <nav>
         <ul>
-          <img src="/img/obelisco.png" alt="logo" width="50" height="50" />
-          <li><Link to='/' className='link'>Bienvenidos</Link></li>
-          <li><Link to='/NuestrosTours' className='link'>Nuestros Tours</Link></li>
-          <li><Link to='/PreguntasFrecuentes' className='link'>Preguntas Frecuentes</Link></li>
-          <li><Link to='/Contacto' className='link'>Contacto</Link></li>
-          {/* carrito integrado como icono */}
+          <li>
+            <img src="/img/obelisco.png" alt="logo" />
+          </li>
+          <li><Link to="/" className="link">Bienvenidos</Link></li>
+          <li><Link to="/NuestrosTours" className="link">Nuestros Tours</Link></li>
+          <li><Link to="/PreguntasFrecuentes" className="link">Preguntas Frecuentes</Link></li>
+          <li><Link to="/Contacto" className="link">Contacto</Link></li>
           <li className="cartnav">
-            <button className="btnCart">
+            <button className="btnCart" onClick={() => setIsCartOpen(!isCartOpen)}>
               <i className="fa-solid fa-cart-shopping"></i>
               {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
             </button>
+            <Cart isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
           </li>
         </ul>
       </nav>
@@ -29,6 +33,8 @@ const Header = () => {
 };
 
 export default Header;
+
+
 
 
 
