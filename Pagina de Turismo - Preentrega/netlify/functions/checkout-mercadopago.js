71964765b0a3ad2093fe8b425e0fa0b25c111516
Analia-Elizabeth-Fernandez <anfer510@gmail.com>
2026-01-15 12:46:31 -0300

    Update checkout-mercadopago.js

--- Pagina de Turismo - Preentrega/netlify/functions/checkout-mercadopago.js
+++ Pagina de Turismo - Preentrega/netlify/functions/checkout-mercadopago.js
@@ -1,8 +1,9 @@
-import mercadopago from "mercadopago";
+import MercadoPago from "mercadopago";
 
-mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN,
+const mercadopago = new MercadoPago(process.env.MP_ACCESS_TOKEN, {
+  locale: 'es-AR'
 });
+
 
 export async function handler(event) {
   const { cart } = JSON.parse(event.body);
